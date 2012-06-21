ddec4532ef39c67a5adcafa490492bc81b4e8680
Phani Kumar Allada <pallad@codeaurora.org>
2012-06-21 15:48:40 +0530

    audio-native: 7x27A: Add Analog FM playback support to native test app
    Enhance the native test app to set FM device and FM volume using
    native test app for 7x27a target.
    
    Change-Id: I2cb017f39c70ec3e65b82b567ae8f57b0f142df4
    CRs-Fixed: 356804

--- audio-native/qdsp5/fm_test.c
+++ audio-native/qdsp5/fm_test.c
@@ -3,7 +3,7 @@
  * Based on native fm test application platform/system/extras/sound/playwav.c
  *
  * Copyright (C) 2008 The Android Open Source Project
- * Copyright (c) 2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2010-2012 Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,13 +30,20 @@
 #include <errno.h>
 #include "audiotest_def.h"
 
+#ifdef AUDIO7X27A
+#define SND_DEVICE_FM_HEADSET 0x23
+#define SND_DEVICE_HEADSET 0x03
+#define MAX_STRING 10
+char DeviceSettingString[MAX_STRING];
+#endif
+
 const char     *dev_file_name;
 int playback_stop = 1;
+int afd;
 
 int fm_play(struct audtest_config *cfg)
 {
 	static int fm_devid_rx;
-	int afd;
 	int ret = 0;
 #ifdef AUDIOV2
 	unsigned short dec_id;
@@ -80,14 +87,24 @@ int fm_play(struct audtest_config *cfg)
 #endif
 
 	printf("start playback\n");
+#ifdef AUDIO7X27A
+		snprintf(DeviceSettingString,MAX_STRING,"%d %d %d"
+			,SND_DEVICE_FM_HEADSET,SND_MUTE_UNMUTED,
+			SND_MUTE_UNMUTED);
+		write(afd, &DeviceSettingString,strlen(DeviceSettingString));
+		printf("FM device: %s set\n",DeviceSettingString);
+#else
 	if (ioctl(afd, AUDIO_START, 0) >= 0) {
+#endif
 		playback_stop = 0;
 		while (playback_stop != 1)
 			sleep(1);
 		printf("end of fm play\n");
+#ifndef AUDIO7X27A
 	} else {
 		printf("fm_play: Unable to start driver\n");
 	}
+#endif
 #if defined(QC_PROP) && defined(AUDIOV2)
 	if (msm_en_device(fm_devid_rx, 0) < 0) {
 		perror("could not disable device\n");
@@ -104,9 +121,10 @@ int fm_play(struct audtest_config *cfg)
 exit:
 #endif
 #ifdef AUDIO7X27A
-	if (ioctl(afd, AUDIO_STOP, 0) < 0) {
-		printf("fm_play: AUDIO_STOP Failed !!\n");
-	}
+		snprintf(DeviceSettingString,MAX_STRING,"%d %d %d"
+			,SND_DEVICE_HEADSET,SND_MUTE_UNMUTED,SND_MUTE_UNMUTED);
+		write(afd, &DeviceSettingString, strlen(DeviceSettingString));
+		printf("default device: %s set\n",DeviceSettingString);
 #endif
 	close(afd);
 	return ret;
@@ -134,7 +152,11 @@ int fm_play_read_params(void)
 	if ((context = get_free_context()) == NULL) {
 		ret_val = -1;
 	} else {
+#ifdef AUDIO7X27A
+		dev_file_name = "/sys/class/misc/msm_snd/device";
+#else
 		dev_file_name = "/dev/msm_fm";
+#endif
 
 		token = strtok(NULL, " ");
 
@@ -159,7 +181,7 @@ int fm_play_control_handler(void *private_data)
 {
 	int  drvfd , ret_val = 0;
 	char *token;
-#if defined(QC_PROP) && defined(AUDIOV2)
+#if (defined(QC_PROP) && defined(AUDIOV2)) || defined(AUDIO7X27A)
 	int volume;
 #endif
 
@@ -174,7 +196,6 @@ int fm_play_control_handler(void *private_data)
 				playback_stop = 1;
 				printf("playback_stop = %d\n",playback_stop);
 			}
-#if defined(QC_PROP) && defined(AUDIOV2)
 			else if (!strcmp(token, "volume")) {
 				int rc;
 				unsigned short dec_id;
@@ -182,6 +203,22 @@ int fm_play_control_handler(void *private_data)
 				if (!memcmp(token, "-value=",
 					(sizeof("-value=") - 1))) {
 					volume = atoi(&token[sizeof("-value=") - 1]);
+#ifdef AUDIO7X27A
+					int fd;
+					snprintf(DeviceSettingString,MAX_STRING,"%d %d %d"
+						,SND_DEVICE_FM_HEADSET,
+						SND_METHOD_VOICE, volume);
+					fd = open("/sys/class/misc/msm_snd/volume", O_WRONLY);
+
+					if (fd < 0) {
+						perror("failed to open device for setting volume");
+						return -1;
+					}
+					printf("Set FM volume  to %d\n",volume);
+					write(fd,&DeviceSettingString,
+						strlen(DeviceSettingString));
+					close(fd);
+#else
 					if (ioctl(drvfd, AUDIO_GET_SESSION_ID, &dec_id)) {
 						perror("could not get decoder session id\n");
 					} else {
@@ -189,9 +226,9 @@ int fm_play_control_handler(void *private_data)
 						rc = msm_set_volume(dec_id, volume);
 						printf("session volume result %d\n", rc);
 					}
+#endif
 				}
 			}
-#endif
 #else
 			token = &token[sizeof("-id=") - 1];
 #endif

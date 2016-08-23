9cc863d799891aba8ac6af6ab761a2a1e6efea10
Rameshkumar Karuppusamy <rameshkr@codeaurora.org>
2016-08-23 10:49:40 +0530

    mm-audio: libalsa-intf: add sleep to avoid CPU usage
    During voice call ARM CPU usage is 99%.In aplay/arec application
    while(1){} is used for hostless PCM use cases, such as voice call
    and AFE loopback. Add sleep inside while loop to avoid CPU usage.
    
    Change-Id: I2c05c6948499ed85fb0082a7c6e5fe6e5bec8e66

--- libalsa-intf/aplay.c
+++ libalsa-intf/aplay.c
@@ -1,6 +1,6 @@
 /*
 ** Copyright 2010, The Android Open-Source Project
-** Copyright (c) 2011-2013, 2015 The Linux Foundation. All rights reserved.
+** Copyright (c) 2011-2013, 2015-2016 The Linux Foundation. All rights reserved.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -316,7 +316,9 @@ static int play_file(unsigned rate, unsigned channels, int fd,
           pcm_close(pcm);
           return -errno;
        }
-        while(1);
+       while(1) {
+          sleep(10);
+       }
     }
 
     remainingData = data_sz;
--- libalsa-intf/arec.c
+++ libalsa-intf/arec.c
@@ -1,6 +1,6 @@
 /*
 ** Copyright 2010, The Android Open-Source Project
-** Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+** Copyright (c) 2011-2013, 2016 The Linux Foundation. All rights reserved.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -268,7 +268,9 @@ int record_file(unsigned rate, unsigned channels, int fd, unsigned count,  unsig
             pcm_close(pcm);
             return -errno;
 	}
-        while(1);
+        while(1) {
+            sleep(10);
+	}
    }
 
     if (flags & PCM_MMAP) {
